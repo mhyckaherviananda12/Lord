b1c5c9bbd565df1cccdbba964c134043a07329ee
mhyckaherviananda12 <95332273+mhyckaherviananda12@users.noreply.github.com>
2022-01-25 22:40:35 +0800

    Update Lord.js

--- Lord.js
+++ Lord.js
@@ -1611,7 +1611,29 @@ if(!q) return reply('Masukkan query lord')
 													}]
 											sendButLocation(from, teks , `Thank for verification 💋\n${namabot}™© | By ${owner_name}`, thumbnail, papako, {contextInfo: { mentionedJid: [sender]}})
 									break
-						case 'autoregis': case 'register':
+case 'p323':
+
+reply(mess.wait)
+bo = args.join(" ")
+ini = await fetchJson(`https://api-alphabot.herokuapp.com/api/downloader/youtube/playmp3?query=${bo}&apikey=Alphabot`)
+sendFileFromUrl(ini.results.result, document, {mimetype: 'audio/mp3', filename: `${ini.results.title}.mp3`, quoted: ftoko})
+break 
+case 'ytp3':
+reply(mess.wait)
+if (args.length ==0)return reply('Link nya Mana?')
+ini_link = args.join(" ")
+anu = await fetchJson(`https://api-alphabot.herokuapp.com/api/downloader/youtube/audio?url=${ini_link}&apikey=Alphabot`)
+get = anu.results
+sendFileFromUrl(anu.results.result, document, {mimetype: 'audio/mp3', filename: `${anu.results.title}.mp3`, quoted: ftoko})
+break
+case 'ytp4':
+reply(mess.wait)
+if (args.length ==0)return reply('Link nya Mana Kak?')
+ini_link = args.join(" ")
+anu = await fetchJson(`https://api-alphabot.herokuapp.com/api/downloader/youtube/video?url=${ini_link}&apikey=Alphabot`)
+get = anu.results
+sendFileFromUrl(anu.results.result, document, {mimetype: 'video/mp4', filename: `${anu.results.title}.mp3`, quoted: ftoko})
+break						case 'autoregis': case 'register':
 									if (!Ofc.key.fromMe && !isOwner && !isCreator) return reply(mess.only.owner)
 									if (args[0] === "on") {
 										if (autoregister === true) return reply(`Mode ${command} telah aktif sebelumnya`)
